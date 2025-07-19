44de71f6efda79f0582f79adda12965ce9848f2a
Juan Pablo Castiglione <juanpablocastiglione01@gmail.com>
2025-07-19 17:55:22 -0300

    Readme

--- packages/backend/index.js
+++ packages/backend/index.js
@@ -6,7 +6,9 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: process.env.ALLOWED_ORIGINS.split(",").map((o) => o.trim()),
+      origin: process.env.ALLOWED_ORIGINS
+      ? process.env.ALLOWED_ORIGINS.split(",").map((o) => o.trim())
+      : true
   })
 );
 
