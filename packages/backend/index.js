e3f5c6f4338c2d11bb2ccc930e011ed0f5ae44fc
Juan Pablo Castiglione <juanpablocastiglione01@gmail.com>
2025-07-19 17:44:53 -0300

    first commit

---
+++ packages/backend/index.js
@@ -0,0 +1,19 @@
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(express.json());
+app.use(
+  cors({
+    origin: process.env.ALLOWED_ORIGINS.split(",").map((o) => o.trim()),
+  })
+);
+
+app.get("/hello", (req, res) => {
+  res.json({ message: "hello world" });
+});
+
+app.listen(process.env.SERVER_PORT, () => {
+  console.log(`Backend escuchando en puerto ${process.env.SERVER_PORT}`);
+});
